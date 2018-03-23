32bb48003a3e83ecd9f95abf6a7c691a12221407
Aidan Harris-Tyrrell <ajudeht@gmail.com>
2018-03-22 18:28:56 -0700

    Site

---
+++ js/index.js
@@ -0,0 +1,19 @@
+//
+// let header = document.querySelector("#header");
+//
+// window.addEventListener('scroll', function(e) {
+//
+// let scrollPos = window.scrollY;
+//
+// console.log(scrollPos);
+// if (scrollPos > 500){
+// if (!header.classList.contains('color')){
+// header.classList.add('color');
+// }
+// } else {
+//   if (header.classList.contains('color')){
+//   header.classList.remove('color');
+//   }
+// }
+//
+// });
